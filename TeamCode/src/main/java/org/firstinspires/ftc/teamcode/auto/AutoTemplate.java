cf784adb25283c2f80f7058bfd4c7bf79906ff08
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-09-07 09:48:46 -0700

    FINALLY PARKING IS DONE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoTemplate.java
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.teamcode.auto;
 
-import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.geometry.BezierLine;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.pedropathing.follower.FollowerConstants;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 import org.firstinspires.ftc.teamcode.util.Parts;
@@ -16,16 +15,16 @@ import org.firstinspires.ftc.teamcode.util.Robot;
 @Autonomous (name = "Please kms", group = "Decode")
 public class AutoTemplate extends LinearOpMode {
     private Follower follower;
-    private Path wtvthefuckitis;
+    private PathChain wtvthefuckitis;
     public static final Pose killingmyself67 = new Pose(63.64, 2.62, Math.toRadians(0));
     public static final Pose leavingline = new Pose(64.23, 37.05, Math.toRadians(0));
     public void runOpMode() throws InterruptedException {
         Parts config = new Parts(hardwareMap); // configure robot
         Robot robot = new Robot(); // configure robot
 
-        //follower = new Follower(Constants.followerConstants, )
+        follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(killingmyself67);
-        paths(); // path builder for park
+        buildPaths(); // path builder for park
 
         waitForStart();
 
@@ -36,9 +35,12 @@ public class AutoTemplate extends LinearOpMode {
         }
     }
 
-    public void paths() {
+    public void buildPaths() {
         // basic parking path
-        wtvthefuckitis = new Path(new BezierLine(killingmyself67, leavingline));
-        wtvthefuckitis.setConstantHeadingInterpolation(killingmyself67.getHeading());
+        //new Path(new BezierLine(killingmyself67, leavingline));
+        wtvthefuckitis = follower.pathBuilder()
+                .addPath(new BezierLine(killingmyself67, leavingline))
+                .setConstantHeadingInterpolation(killingmyself67.getHeading())
+                .build();
     }
 }
