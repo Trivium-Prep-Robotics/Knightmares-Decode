c65871e33ba497ec41faa0c446ee5616c948c163
NotTacos <87448560+PhysicalDrive@users.noreply.github.com>
2025-09-06 21:02:40 -0700

    basic park and pedro config! jacob don't look at the comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoTemplate.java
@@ -1,23 +1,41 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import com.pedropathing.paths.Path;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.geometry.BezierLine;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.pedropathing.follower.FollowerConstants;
 
 import org.firstinspires.ftc.teamcode.util.Parts;
 import org.firstinspires.ftc.teamcode.util.Robot;
 
-/**
- * This is the general structure for an autonomous
- */
 
-@Autonomous (name = "Net side", group = "old Auto")
-//@Disabled
+@Autonomous (name = "Please kms", group = "Decode")
 public class AutoTemplate extends LinearOpMode {
+    private Follower follower;
+    private Path wtvthefuckitis;
+    public static final Pose killingmyself67 = new Pose(63.64, 2.62, Math.toRadians(0));
     public void runOpMode() throws InterruptedException {
         Parts config = new Parts(hardwareMap); // configure robot
         Robot robot = new Robot(); // configure robot
+        
+        follower.setStartingPose(killingmyself67);
+        paths(); // path builder for park
 
         waitForStart();
-        
+
+        // go park which is going after the line and free 3 points!!!!!!!!
+        follower.followPath(wtvthefuckitis);
+        while (follower.isBusy()) {
+            follower.update();
+        }
+    }
+
+    public void paths() {
+        // basic parking path
+        wtvthefuckitis = new Path(new BezierLine(new Pose(63.64, 2.62, Math.toRadians(0)), new Pose(64.23, 37.05, Math.toRadians(0))));
+        wtvthefuckitis.setConstantHeadingInterpolation(Math.toRadians(0));
     }
 }
