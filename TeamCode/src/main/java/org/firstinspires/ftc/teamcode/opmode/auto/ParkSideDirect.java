fa528ccd23c0d3bf9c214d17b4651f2c21521c50
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-09-09 16:32:42 -0700

    pathing for today

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/ParkSideDirect.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/ParkSideDirect.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
+import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.pedropathing.follower.Follower;
@@ -7,6 +8,7 @@ import com.pedropathing.geometry.Pose;
 import com.pedropathing.geometry.BezierLine;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.common.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.common.util.Shooter;
 
 import org.firstinspires.ftc.teamcode.common.util.Parts;
 import org.firstinspires.ftc.teamcode.common.util.Robot;
@@ -15,9 +17,14 @@ import org.firstinspires.ftc.teamcode.common.util.Robot;
 @Autonomous (name = "ParkSideDirect", group = "Decode")
 public class ParkSideDirect extends LinearOpMode {
     private Follower follower;
-    private PathChain parking;
-    public static final Pose startingpose = new Pose(63.64, 2.62, Math.toRadians(0));
-    public static final Pose leavingline = new Pose(64.23, 37.05, Math.toRadians(0));
+    private PathChain scoring;
+    private PathChain goingtohuman;
+    private PathChain scoring2;
+    private PathChain goingtohuman2;
+    private PathChain scoring3;
+    public static final Pose startingpose = new Pose(56, 8, Math.toRadians(0));
+    public static final Pose scorepose = new Pose(28.14, 120.52, Math.toRadians(0));
+    public static final Pose humanplayer = new Pose(12.63, 11.52, Math.toRadians(0));
     public void runOpMode() throws InterruptedException {
         Parts config = new Parts(hardwareMap); // configure robot
         Robot robot = new Robot(); // configure robot
@@ -29,18 +36,64 @@ public class ParkSideDirect extends LinearOpMode {
         waitForStart();
 
         // go park which is going after the line and free 3 points!!!!!!!!
-        follower.followPath(parking);
+        follower.followPath(scoring);
         while (follower.isBusy()) {
             follower.update();
         }
+
+        Shooter.shootauto();
+
+        follower.followPath(goingtohuman);
+        while (follower.isBusy()) {
+            follower.update();
+        }
+
+        follower.followPath(scoring2);
+        while (follower.isBusy()) {
+            follower.update();
+        }
+
+        Shooter.shootauto();
+
+        follower.followPath(goingtohuman2);
+        while (follower.isBusy()) {
+            follower.update();
+        }
+
+        follower.followPath(scoring3);
+        while (follower.isBusy()) {
+            follower.update();
+        }
+
+        Shooter.shootauto();
     }
 
     public void buildPaths() {
         // basic parking path
         //new Path(new BezierLine(killingmyself67, leavingline));
-        parking = follower.pathBuilder()
-                .addPath(new BezierLine(startingpose, leavingline))
+        scoring = follower.pathBuilder()
+                .addPath(new BezierLine(startingpose, scorepose))
                 .setConstantHeadingInterpolation(startingpose.getHeading())
+                .build();
+
+        goingtohuman = follower.pathBuilder()
+                .addPath(new BezierLine(scorepose, humanplayer))
+                .setConstantHeadingInterpolation(scorepose.getHeading())
+                .build();
+
+        scoring2 = follower.pathBuilder()
+                .addPath(new BezierLine(humanplayer, scorepose))
+                .setConstantHeadingInterpolation(humanplayer.getHeading())
+                .build();
+
+        goingtohuman2 = follower.pathBuilder()
+                .addPath(new BezierLine(scorepose, humanplayer))
+                .setConstantHeadingInterpolation(scorepose.getHeading())
+                .build();
+
+        scoring3 = follower.pathBuilder()
+                .addPath(new BezierLine(humanplayer, scorepose))
+                .setConstantHeadingInterpolation(humanplayer.getHeading())
                 .build();
     }
 }
