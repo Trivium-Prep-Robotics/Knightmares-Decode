d0275cc13dbc9878d655d7b214841fe96f7bee1c
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-09-08 18:43:01 -0700

    remove camera since we don't need it for now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleOp/teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleOp/teleOp.java
@@ -4,18 +4,11 @@ import dev.nextftc.ftc.NextFTCOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.common.pedroPathing.Constants;
-import org.openftc.apriltag.AprilTagDetection;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 
 
 import org.firstinspires.ftc.teamcode.common.util.Parts;
 import org.firstinspires.ftc.teamcode.common.util.Robot;
-import org.firstinspires.ftc.teamcode.common.util.AprilTagDetectionPipeline;
 
 
 /**
@@ -24,43 +17,12 @@ import org.firstinspires.ftc.teamcode.common.util.AprilTagDetectionPipeline;
  */
 @TeleOp (name = "teleOp", group = "TELEOP")
 public class teleOp extends NextFTCOpMode {
-    OpenCvCamera camera;
     private Follower follower;
     private final Pose startingpose = new Pose(0,0,Math.toRadians(0));
-    AprilTagDetectionPipeline aprilTagDetectionPipeline;
-
-    // calibration for C920 webcam if you have different camera then uhh change it ig??
-    double fx = 578.272;
-    double fy = 578.272;
-    double cx = 402.145;
-    double cy = 221.506;
-
-    // UNITS ARE METERS
-    double tagsize = 0.166;
 
     public void onInit() {
         Parts config = new Parts(hardwareMap); // configure robot
         Robot robot = new Robot(); // configure robot
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
-
-        camera.setPipeline(aprilTagDetectionPipeline);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
-            @Override
-            public void onOpened()
-            {
-                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
-            }
-
-            @Override
-            public void onError(int errorCode)
-            {
-                telemetry.addData("Camera Error: ", errorCode);
-                telemetry.update();
-            }
-        });
 
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(startingpose);
